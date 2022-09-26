6fc84d2426ebe04c065770e61754d0be9f1d19ad
Anna-1902 <annaipsen19@gmail.com>
2022-09-26 19:23:18 +0200

    create alphabet buttons

---
+++ scripts/hangman.js
@@ -0,0 +1,19 @@
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y', 'z', 'æ', 'ø', 'å'];
+
+function createButtons(){
+    let buttons = document.getElementById('buttons'); 
+    let letters = document.createElement('ul');
+
+    for (let i = 0; i < alphabet.length; i++) {
+        letters.id = 'alphabet';
+        let list = document.createElement('li');
+        list.id = 'letter'
+        list.innerHTML = alphabet[i];
+
+        buttons.appendChild(letters);
+        letters.appendChild(list);
+    }
+}
+createButtons()
