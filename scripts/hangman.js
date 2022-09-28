bcb96141162ba1c3f86688ae4be7dd615bfd47c4
Anna-1902 <annaipsen19@gmail.com>
2022-09-28 08:20:50 +0200

    klik-funktion og bogstaver der bliver synlige, når der gættes rigtigt

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -3,6 +3,7 @@ const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
 't', 'u', 'v', 'w', 'x', 'y', 'z', 'æ', 'ø', 'å'];
 
+
 function createButtons(){
     let buttons = document.getElementById('buttons'); 
     let letters = document.createElement('ul');
@@ -11,6 +12,7 @@ function createButtons(){
         letters.id = 'alphabet';
         let list = document.createElement('li');
         list.id = 'letter'
+        list.addEventListener('click', clickFunction)
         list.innerHTML = alphabet[i];
 
         buttons.appendChild(letters);
@@ -19,8 +21,77 @@ function createButtons(){
 };
 createButtons();
 
-//generate word
-const words = ['pasta', 'masseødelæggelsesvåben', 'danmark', 'webudvikling', 'verdensmesterskab', 'brød']; 
+//generate word and counter
+const wordsArray = ['pasta', 'brandbil', 'danmark', 'webudvikling', 'verdensmesterskab', 'antifrost', 'skuffe', 
+'irritation', 'kampsport', 'tusindben', 'spredning']; 
+const randomWord = wordsArray[(Math.floor(Math.random() * wordsArray.length))];
+let wordDisplay = []
+var randomWordArray = randomWord.split("");
+
+
+function generateWord(){
+    for (var i = 0; i < randomWordArray.length; i++) {
+        wordDisplay.push("_");
+    }
+    
+    outputWord();
+};
+generateWord();
+
+function outputWord (){
+    document.getElementById('word').innerHTML = wordDisplay.join(' ');
+}
+
+let counter = 0;
+let lives = 10;
+
+function showCounter() {
+    document.getElementById('counter').innerHTML = `Du har brugt ${counter} tur(e)`;
+};
+showCounter();
+
+function showLives() {
+    document.getElementById('lives').innerHTML = `Du har ${lives} liv tilbage`;
+};
+showLives();
+
+
+function check () {
+    if (JSON.stringify(wordDisplay) === JSON.stringify(randomWordArray)) {
+        document.getElementById('win').innerHTML = 'tillykke du har vundet!';
+    }
+    else if (lives == 0) {
+        document.getElementById('win').innerHTML = 'GAME OVER!';
+    };
+}
+
+// OnClick Function
+let guessCount = 0;
+
+function clickFunction () {
+    let guess = this.innerHTML;
+    this.setAttribute('class', 'usedLetter');
+    for (var i = 0; i < wordDisplay.length; i++) {
+        if (randomWordArray[i] === guess) {
+            wordDisplay[i] = guess
+            guessCount += 1
+            outputWord()
+        };
+    };
+
+    if (guess === 0) {
+        lives -= 1;
+        counter += 1;
+        guessCount = 0;
+    } else {
+        counter += 1;
+    };
+
+    showLives();
+    showCounter();
+    check();
+};
+
 
 
 
