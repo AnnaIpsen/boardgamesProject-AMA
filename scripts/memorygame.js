e4ef3b1a884561d15350f517247246279f3b9363
Anna-1902 <annaipsen19@gmail.com>
2022-09-29 10:11:06 +0200

    memorygame done

--- scripts/memorygame.js
+++ scripts/memorygame.js
@@ -1,7 +1,10 @@
-//generer kort
 const cardArray = [];
 memoryTiles = 50
+let chosenCards = []
+let cardsWon = 0
+let cardsId = []
 
+//generer kort
 for (let i = 0; i < memoryTiles; i++) {
    cardArray.push(i)
    cardArray.push(i)
@@ -11,49 +14,87 @@ cardArray.sort(() => 0.5 - Math.random())
 
 const grid = document.getElementById('grid')
 
-
 function createBoard() {
     for (let i = 0; i < cardArray.length; i++){
         const card = document.createElement('div');
         card.setAttribute('id', i)
         card.innerHTML = `<p>${cardArray[i]}</p>`
         let number = card.firstElementChild;
-        number.setAttribute('id', 'hidden')
+        number.setAttribute('id', 'show')
         card.addEventListener('click', flip);
         grid.appendChild(card);
     }
 }
 createBoard()
 
-//check kort
-let chosenCards = []
-let cardsWon = []
+//infoBoards
+let min = 0;
+let sec = 0;
 
+const myInterval = setInterval(()=> {
+        sec++;
+        if(sec === 60) {
+            sec = 0;
+            min++;
+        };
+        document.getElementById('timer').innerHTML = `Tid brugt: ${min}min ${sec<10 ? "0" + sec : sec}sek`
+    }, 1000);
+
+function stopTimer () {
+    clearInterval(myInterval)
+
+    document.getElementById('winner').innerHTML = "Tillykke du har vundet!"
+}
+
+function cardsWonShow() {
+    document.getElementById('set').innerHTML = `Antal sæt vundet: ${cardsWon}`
+
+    if(cardsWon === 50) {
+        stopTimer()
+    }
+}
+
+cardsWonShow()
+
+//check kort
 function checkMatch () {
     let message = document.getElementById('message')
-    let optionOneId = chosenCards[0]
-    let optionTwoId = chosenCards[1]
+    let optionOneId = cardsId[0]
+    let optionTwoId = cardsId[1]
 
     if (chosenCards[0] === chosenCards[1]){
-        cardsWon.push(chosenCards)
-        message.innerHTML = 'Du fik et match, tillykke!'
+        message.innerHTML = 'Du fik et match, tillykke!';
+        
+        document.getElementById(optionOneId).setAttribute('id', 'won');
+        document.getElementById(optionTwoId).setAttribute('id', 'won');
+
+
+        chosenCards = []
+        cardsId = []
+        cardsWon += 1
+
+        cardsWonShow()
+
     } else {
         message.innerHTML = 'Prøv igen'
 
-        console.log(chosenCards)
-        let number1 = document.getElementById(optionOneId).firstElementChild;
-        let number2 = document.getElementById(optionTwoId).firstElementChild;
-        number1.setAttribute('id', 'hidden')
-        number2.setAttribute('id', 'hidden')
-        
-        console.log(number1)
-        console.log(number2)
+        setTimeout(() => {
+            let number1 = document.getElementById(optionOneId).firstElementChild;
+            let number2 = document.getElementById(optionTwoId).firstElementChild;
+            number1.setAttribute('id', 'hidden')
+            number2.setAttribute('id', 'hidden')
+            
+    
+            chosenCards = []
+            cardsId = []
+        }, 750)
     }
 }
 
 function flip() {
     let cardId = this.getAttribute('id')
     chosenCards.push(cardArray[cardId])
+    cardsId.push(cardId)
     let number = document.getElementById(cardId).firstElementChild;
     number.setAttribute('id', 'show')
 
